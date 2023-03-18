aad246cf3e6a9200655c8a0fbbaf7bbd7f60dae1
Saymon Damásio <saymon.damasio95@gmail.com>
2023-03-17 21:35:51 -0300

    chore: config fonts

--- twind.config.ts
+++ twind.config.ts
@@ -111,7 +111,7 @@ const options: Omit<Options, "selfURL"> = {
       },
     },
     fontFamily: {
-      sans: ["Albert Sans", "sans-serif"],
+      sans: ["Open Sans", "sans-serif"],
       serif: ["inherit", "serif"],
     },
     screens: {
